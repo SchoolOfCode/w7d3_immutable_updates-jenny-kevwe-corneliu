bfaf773cc4d049e2e0bb7deb757f57ed62c01eba
Jenny Taylor <jennytaylor82@gmail.com>
2022-01-06 12:35:41 +0000

    objects task completed

--- index.js
+++ index.js
@@ -32,7 +32,10 @@ export function removeItem(array, index) {
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+return {...object, name: newName}
+
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
@@ -40,7 +43,9 @@ export function updateName(object, newName) {}
 //     toggleTeaStatus({ name: "Abe", needsACupOfTea: false })
 // should give back:
 //     { name: "Abe", needsACupOfTea: true }
-export function toggleTeaStatus(object) {}
+export function toggleTeaStatus(object) {
+    return {...object, needsACupOfTea: !object.needsACupOfTea}
+}
 
 // Combo Time!!
 
@@ -50,4 +55,7 @@ export function toggleTeaStatus(object) {}
 //    toggleListItemCompleted([{ task: "Cooking", completed: true }, { task: "Walking", completed: false }], 1)
 // should give back:
 //    [{ task: "Cooking", completed: true }, { task: "Walking", completed: true }]
-export function toggleListItemCompleted(array, index) {}
+export function toggleListItemCompleted(array, index) {
+    return [...array.slice(0, index), {...array[index], completed: !array[index].completed}, ...array.slice(index+1)]
+
+}
